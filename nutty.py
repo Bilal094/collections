983b46331f0f74339ac0d7d5319f39d952ea89aa
Bilal094 <99066318@mydavinci.nl>
2022-02-14 09:57:32 +0100

    List en dicitonary komen nu overeen met elkaar

--- nutty.py
+++ nutty.py
@@ -1,16 +1,8 @@
 import random
-Kleuren = ["oranje", "blauw", "groen", "bruin"]
-ZakList = []
+Kleuren = ['oranje', 'blauw', 'groen', 'bruin']
 ZakDictionary = {}
+ZakList = []
 # --------------------------------------------------------------------
-
-def List(aantal):
-    for x in range(aantal):
-        Inhoud = random.choice(Kleuren)
-        ZakList.append(Inhoud)
-        
-    return ZakList
-
 def Dictionary(aantal):
     for x in range(aantal):
         Inhoud = random.choice(Kleuren)
@@ -20,13 +12,28 @@ def Dictionary(aantal):
             ZakDictionary[Inhoud] = 1
     return ZakDictionary
 
+def Lijst(dict,lijst):
+    oranjeAantal = dict.get('oranje')
+    for a in range(oranjeAantal):
+        lijst.append(Kleuren[0])
+    blauwAantal = dict.get('blauw')
+    for b in range(blauwAantal):
+        lijst.append(Kleuren[1])
+    groenAantal = dict.get('groen')
+    for c in range(groenAantal):
+        lijst.append(Kleuren[2])
+    bruinAantal = dict.get('bruin')
+    for d in range(bruinAantal):
+        lijst.append(Kleuren[3])
+    return ZakList
+
 def Sorteren(lijst):
     lijst.sort()
     return lijst
 
-
-
 Aantal = int(input('Hoeveel M&Ms wil je hebben? '))
-List(Aantal)
-print(Sorteren(ZakList))
-print(Dictionary(Aantal))
+Dictionary(Aantal)
+Lijst(ZakDictionary, ZakList)
+Sorteren(ZakList)
+print(ZakList)
+print(ZakDictionary)
